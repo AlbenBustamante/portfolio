9951a3d6496b3d15d7f63165a805e2af7ab530cd
Alben Bustamante <albenbustamante@gmail.com>
2025-07-02 22:40:05 -0500

    update: navbar and create main container component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, RouterOutlet],
+  imports: [NavbarComponent, RouterOutlet, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../components/button/button.component';
+import { MainContainerComponent } from '../../components/main-container/main-container.component';
 
 @Component({
   selector: 'app-home',
-  imports: [ButtonComponent],
+  imports: [MainContainerComponent, ButtonComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export default class HomeComponent {}
+export default class HomeComponent {
+  alert() {
+    alert('jkfdjksf');
+  }
+}
