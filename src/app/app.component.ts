02607f4e748dbcc6b53ef3650b1dd728816f3415
Alben Bustamante <albenbustamante@gmail.com>
2025-07-05 13:22:58 -0500

    define: structure pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import HomeComponent from './features/home/home.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ContactComponent } from './features/contact/contact.component';
+import { ProjectsComponent } from './features/projects/projects.component';
+import { ResumeComponent } from './features/resume/resume.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, HomeComponent, SidebarComponent],
+  imports: [
+    NavbarComponent,
+    SidebarComponent,
+    ContactComponent,
+    ProjectsComponent,
+    ResumeComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
