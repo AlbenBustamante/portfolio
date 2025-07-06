f2f7c9d3a1b84d70c0519123755c2387e84e15b4
Alben Bustamante <albenbustamante@gmail.com>
2025-07-05 19:24:02 -0500

    add: skill icons

--- src/app/features/landing/resume/resume.component.ts
+++ src/app/features/landing/resume/resume.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
-import { LandingSubtitleComponent } from '../components/landing-subtitle/landing-subtitle.component';
 import { LandingSectionContainerComponent } from '../components/landing-section-container/landing-section-container.component';
 import { EducationComponent } from './components/education/education.component';
 import { ExperienceComponent } from './components/experience/experience.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 @Component({
   selector: 'app-resume',
   imports: [
-    LandingSubtitleComponent,
     LandingSectionContainerComponent,
     EducationComponent,
     ExperienceComponent,
+    SkillsComponent,
   ],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.css',
