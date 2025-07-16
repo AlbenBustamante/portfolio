35db33627665b2fde5373c184f29a8db33fa4154
Alben Bustamante <albenbustamante@gmail.com>
2025-07-16 15:15:13 -0500

    add: docker as skill

--- src/app/components/accessibility/accessibility.component.ts
+++ src/app/components/accessibility/accessibility.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { MoonIconComponent } from '../icons/moon-icon/moon-icon.component';
 import { GlobeIconComponent } from '../icons/globe-icon/globe-icon.component';
-import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-accessibility',
-  imports: [MoonIconComponent, GlobeIconComponent, ButtonComponent],
+  imports: [MoonIconComponent, GlobeIconComponent],
   templateUrl: './accessibility.component.html',
   styleUrl: './accessibility.component.css',
 })
