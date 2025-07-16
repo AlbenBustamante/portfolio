aee0d9d4a35c353256d193ef4d5d40b43f6b2174
Alben Bustamante <albenbustamante@gmail.com>
2025-07-16 16:38:42 -0500

    set: default translator config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,23 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
 import { AccessibilityComponent } from './components/accessibility/accessibility.component';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, RouterOutlet, AccessibilityComponent],
+  imports: [
+    TranslateModule,
+    NavbarComponent,
+    RouterOutlet,
+    AccessibilityComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private readonly _translate: TranslateService) {
+    this._translate.addLangs(['es', 'en']);
+    this._translate.setDefaultLang('es');
+    this._translate.use(this._translate.getBrowserLang() || 'es');
+  }
+}
