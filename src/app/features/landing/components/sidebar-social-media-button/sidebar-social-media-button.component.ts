5672773928957d8699385e212161d73f6ca2a6b8
Alben Bustamante <albenbustamante@gmail.com>
2025-07-17 17:38:31 -0500

    update: social media buttons

--- src/app/features/landing/components/sidebar-social-media-button/sidebar-social-media-button.component.ts
+++ src/app/features/landing/components/sidebar-social-media-button/sidebar-social-media-button.component.ts
@@ -1,12 +1,14 @@
 import { Component, Input } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-sidebar-social-media-button',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './sidebar-social-media-button.component.html',
   styleUrl: './sidebar-social-media-button.component.css',
 })
 export class SidebarSocialMediaButtonComponent {
-  @Input({ required: true }) text!: string;
+  @Input({ required: true }) value!: string;
   @Input({ required: true }) socialName!: string;
+  @Input({ required: true }) url!: string;
 }
