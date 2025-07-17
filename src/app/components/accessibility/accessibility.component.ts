3a421748777f3333f67ad12e0376541fbe749172
Alben Bustamante <albenbustamante@gmail.com>
2025-07-17 11:43:58 -0500

    update: languages design

--- src/app/components/accessibility/accessibility.component.ts
+++ src/app/components/accessibility/accessibility.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { MoonIconComponent } from '../icons/moon-icon/moon-icon.component';
 import { GlobeIconComponent } from '../icons/globe-icon/globe-icon.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-accessibility',
-  imports: [MoonIconComponent, GlobeIconComponent],
+  imports: [MoonIconComponent, GlobeIconComponent, NgClass],
   templateUrl: './accessibility.component.html',
   styleUrl: './accessibility.component.css',
 })
-export class AccessibilityComponent {}
+export class AccessibilityComponent {
+  readonly showLanguages = signal<boolean>(false);
+
+  toggleShowLanguages() {
+    this.showLanguages.update((show) => !show);
+  }
+}
