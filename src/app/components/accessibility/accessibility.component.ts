8ca4534edfcab3b98094addeff7b4e6b8f7d565a
Alben Bustamante <albenbustamante@gmail.com>
2025-07-17 11:56:49 -0500

    define: logic to set a language

--- src/app/components/accessibility/accessibility.component.ts
+++ src/app/components/accessibility/accessibility.component.ts
@@ -2,6 +2,7 @@ import { Component, signal } from '@angular/core';
 import { MoonIconComponent } from '../icons/moon-icon/moon-icon.component';
 import { GlobeIconComponent } from '../icons/globe-icon/globe-icon.component';
 import { NgClass } from '@angular/common';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-accessibility',
@@ -12,7 +13,13 @@ import { NgClass } from '@angular/common';
 export class AccessibilityComponent {
   readonly showLanguages = signal<boolean>(false);
 
+  constructor(private readonly _translateService: TranslateService) {}
+
   toggleShowLanguages() {
     this.showLanguages.update((show) => !show);
+  }
+
+  setLanguage(language: 'es' | 'en') {
+    this._translateService.use(language);
   }
 }
