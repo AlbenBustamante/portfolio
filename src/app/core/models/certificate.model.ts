53080639f19445881fefd42686ca66b1072ed173
Alben Bustamante <albenbustamante@gmail.com>
2025-08-03 20:29:09 -0500

    update: certificate design

--- src/app/core/models/certificate.model.ts
+++ src/app/core/models/certificate.model.ts
@@ -2,4 +2,6 @@ export interface Certificate {
   title: string;
   url: string;
   institute: string;
+  imagePath: string;
+  imageAlt: string;
 }
--- src/app/features/landing/resume/certificates/certificates.component.ts
+++ src/app/features/landing/resume/certificates/certificates.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { ResumeSectionComponent } from '../components/resume-section/resume-section.component';
 import { CertificateService } from '@core/services/certificate.service';
-import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-certificates',
-  imports: [ResumeSectionComponent, JsonPipe],
+  imports: [ResumeSectionComponent],
   templateUrl: './certificates.component.html',
   styleUrl: './certificates.component.css',
 })
