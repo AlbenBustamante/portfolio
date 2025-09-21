852edc92346879025f4b1d4e8dfe15e883b81a0d
Alben Bustamante <albenbustamante@gmail.com>
2025-09-21 12:34:30 -0500

    add: close menu icon; define: logic to open and close the navbar

--- src/app/features/me/components/navbar/navbar.component.ts
+++ src/app/features/me/components/navbar/navbar.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { MenuIconComponent } from '../icons/menu-icon/menu-icon.component';
 import { NavbarItemComponent } from '../navbar-item/navbar-item.component';
+import { NgClass } from '@angular/common';
+import { CloseMenuIconComponent } from '../icons/close-menu-icon/close-menu-icon.component';
 
 @Component({
   selector: 'app-navbar',
-  imports: [MenuIconComponent, NavbarItemComponent],
+  imports: [
+    MenuIconComponent,
+    CloseMenuIconComponent,
+    NavbarItemComponent,
+    NgClass,
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  readonly showNavbar = signal<boolean>(false);
+}
