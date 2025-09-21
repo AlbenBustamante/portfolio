5c0609f0b666a2b42fa1b5cadc57f4ca71b82394
Alben Bustamante <albenbustamante@gmail.com>
2025-09-21 16:28:53 -0500

    add: social icons to home

--- src/app/features/me/home/home.component.ts
+++ src/app/features/me/home/home.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { SocialButtonComponent } from '../components/social-button/social-button.component';
+import { GithubIconComponent } from '../components/icons/github-icon/github-icon.component';
+import { LinkedinIconComponent } from '../components/icons/linkedin-icon/linkedin-icon.component';
+import { WhatsappIconComponent } from '../components/icons/whatsapp-icon/whatsapp-icon.component';
+import { MailIconComponent } from '../components/icons/mail-icon/mail-icon.component';
+import { DiscordIconComponent } from '../components/icons/discord-icon/discord-icon.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    SocialButtonComponent,
+    GithubIconComponent,
+    LinkedinIconComponent,
+    WhatsappIconComponent,
+    MailIconComponent,
+    DiscordIconComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
