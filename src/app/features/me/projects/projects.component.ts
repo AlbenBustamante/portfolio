aa32ef9ae76eb26308c3df4295d4d1331a7993bd
Alben Bustamante <albenbustamante@gmail.com>
2025-09-22 08:19:58 -0500

    add: projects section

--- src/app/features/me/projects/projects.component.ts
+++ src/app/features/me/projects/projects.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { TitleComponent } from '../components/title/title.component';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [TitleComponent],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css',
 })
