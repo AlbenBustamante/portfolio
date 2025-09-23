90d93c2522498d64a7d533ef462f4dc11bbca6d1
Alben Bustamante <albenbustamante@gmail.com>
2025-09-23 17:05:13 -0500

    define: hire me card as shared component; update: hire me page responsive design

--- src/app/features/me/hire-me/hire-me.component.ts
+++ src/app/features/me/hire-me/hire-me.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { FormComponent } from './components/form/form.component';
 import { TitleComponent } from '../components/title/title.component';
+import { SocialMediaComponent } from '../components/social-media/social-media.component';
+import { SubtitleComponent } from '../components/subtitle/subtitle.component';
+import { CardComponent } from '@components/card/card.component';
 
 @Component({
   selector: 'app-hire-me',
-  imports: [FormComponent, TitleComponent],
+  imports: [
+    FormComponent,
+    TitleComponent,
+    SocialMediaComponent,
+    SubtitleComponent,
+    CardComponent,
+  ],
   templateUrl: './hire-me.component.html',
   styleUrl: './hire-me.component.css',
 })
