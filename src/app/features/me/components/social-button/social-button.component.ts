5455697015d7bf71ee5914e7dd290c358d91ae84
Alben Bustamante <albenbustamante@gmail.com>
2025-09-23 17:52:47 -0500

    add: 'display name' property to social media buttons

--- src/app/features/me/components/social-button/social-button.component.ts
+++ src/app/features/me/components/social-button/social-button.component.ts
@@ -1,12 +1,15 @@
+import { NgClass } from '@angular/common';
 import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-social-button',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './social-button.component.html',
   styleUrl: './social-button.component.css',
 })
 export class SocialButtonComponent {
   readonly url = input.required<string>();
   readonly socialName = input.required<string>();
+  readonly displayName = input<string>('');
+  readonly axis = input<'x' | 'y'>('x');
 }
