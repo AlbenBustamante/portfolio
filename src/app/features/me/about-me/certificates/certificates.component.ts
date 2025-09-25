2d5a9fed886b87217f3940c9a5e93d25f2e09eba
Alben Bustamante <albenbustamante@gmail.com>
2025-09-25 12:27:54 -0500

    add: dynamic background to certificates filter

--- src/app/features/me/about-me/certificates/certificates.component.ts
+++ src/app/features/me/about-me/certificates/certificates.component.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { SubtitleComponent } from '@components/subtitle/subtitle.component';
 import { CertificateComponent } from './components/certificate/certificate.component';
 import { CertificateStore } from './certificates.store';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-certificates',
-  imports: [SubtitleComponent, CertificateComponent],
+  imports: [NgClass, SubtitleComponent, CertificateComponent],
   templateUrl: './certificates.component.html',
   styleUrl: './certificates.component.css',
   providers: [CertificateStore],
