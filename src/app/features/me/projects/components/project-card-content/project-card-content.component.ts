0082a48ffc1cdc7d2e0bfb916cec729e93b06335
Alben Bustamante <albenbustamante@gmail.com>
2025-09-26 17:40:38 -0500

    update: projects responsive design

--- src/app/features/me/projects/components/project-card-content/project-card-content.component.ts
+++ src/app/features/me/projects/components/project-card-content/project-card-content.component.ts
@@ -1,12 +1,14 @@
+import { NgClass } from '@angular/common';
 import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-project-card-content',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './project-card-content.component.html',
   styleUrl: './project-card-content.component.css',
 })
 export class ProjectCardContentComponent {
+  readonly axis = input<'x' | 'y'>('y');
   readonly description = input.required<string>();
   readonly category = input.required<string>();
   readonly skills = input.required<string[]>();
--- src/app/features/me/projects/components/project-card/project-card.component.ts
+++ src/app/features/me/projects/components/project-card/project-card.component.ts
@@ -2,22 +2,23 @@ import { Component, input } from '@angular/core';
 import { OpenLiveDemoComponent } from '../open-live-demo/open-live-demo.component';
 import { CardComponent } from '@components/card/card.component';
 import { SourceCodeButtonComponent } from '../source-code-button/source-code-button.component';
-import { ProjectCardHeaderComponent } from '../project-card-header/project-card-header.component';
 import { ProjectCardContentComponent } from '../project-card-content/project-card-content.component';
 import { Project } from '../../models/project.model';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-project-card',
   imports: [
+    NgClass,
     OpenLiveDemoComponent,
     CardComponent,
     SourceCodeButtonComponent,
-    ProjectCardHeaderComponent,
     ProjectCardContentComponent,
   ],
   templateUrl: './project-card.component.html',
   styleUrl: './project-card.component.css',
 })
 export class ProjectCardComponent {
+  readonly axis = input<'x' | 'y'>('y');
   readonly project = input.required<Project>();
 }
