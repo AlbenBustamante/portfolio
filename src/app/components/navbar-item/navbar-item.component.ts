22bdf525b486f4eceadae979cb5e7493879b0c80
Alben Bustamante <albenbustamante@gmail.com>
2025-09-27 13:18:08 -0500

    feat: new effects to navbar

--- src/app/components/navbar-item/navbar-item.component.ts
+++ src/app/components/navbar-item/navbar-item.component.ts
@@ -1,9 +1,10 @@
+import { NgClass } from '@angular/common';
 import { Component, input, output } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar-item',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, NgClass],
   templateUrl: './navbar-item.component.html',
   styleUrl: './navbar-item.component.css',
 })
@@ -11,4 +12,5 @@ export class NavbarItemComponent {
   readonly onClick = output<void>();
   readonly text = input.required<string>();
   readonly route = input.required<string>();
+  readonly adjust = input.required<boolean>();
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, HostListener, signal } from '@angular/core';
 import { MenuIconComponent } from '../icons/menu-icon/menu-icon.component';
 import { NavbarItemComponent } from '../navbar-item/navbar-item.component';
 import { NgClass } from '@angular/common';
@@ -16,5 +16,30 @@ import { CloseMenuIconComponent } from '../icons/close-menu-icon/close-menu-icon
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
+  readonly adjust = signal<boolean>(false);
   readonly showNavbar = signal<boolean>(false);
+
+  ngOnInit() {
+    this._setAdjust();
+  }
+
+  @HostListener('window:scroll')
+  onScroll() {
+    this._setAdjust();
+  }
+
+  @HostListener('mouseenter')
+  onMouseEnter() {
+    this.adjust.set(false);
+  }
+
+  @HostListener('mouseleave')
+  onMouseLeave() {
+    this._setAdjust();
+  }
+
+  private _setAdjust() {
+    const scrollY = window.scrollY;
+    this.adjust.set(scrollY > 80);
+  }
 }
