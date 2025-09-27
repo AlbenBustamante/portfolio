9417b71523688e615e58a53637487cc44c4bbb2d
Alben Bustamante <albenbustamante@gmail.com>
2025-09-27 12:40:18 -0500

    update: about-me gaps, page title

--- src/app/features/me/about-me/education/education.component.ts
+++ src/app/features/me/about-me/education/education.component.ts
@@ -2,11 +2,10 @@ import { Component, inject } from '@angular/core';
 import { SubtitleComponent } from '@components/subtitle/subtitle.component';
 import { EducationStore } from './education.store';
 import { EducationItemComponent } from './components/education-item/education-item.component';
-import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-education',
-  imports: [SubtitleComponent, EducationItemComponent, NgClass],
+  imports: [SubtitleComponent, EducationItemComponent],
   templateUrl: './education.component.html',
   styleUrl: './education.component.css',
   providers: [EducationStore],
