30a9bdc703083cd84eb0e0272c1d9aba45e5c0a2
Alben Bustamante <albenbustamante@gmail.com>
2025-09-29 10:29:04 -0500

    fix: navbar and routes bugs

--- src/app/features/old/old.routes.ts
+++ src/app/features/old/old.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 
 export default [
   {
+    path: '',
+    redirectTo: '/',
+    pathMatch: 'full',
+  },
+  {
     path: 'v1',
     loadChildren: () => import('./v1/v1.routes'),
   },
