54dcde08a4e4abf5cca0da3f681c062827a3ced6
Alben Bustamante <albenbustamante@gmail.com>
2025-10-30 17:28:36 -0500

    update: button styles

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -12,6 +12,9 @@ export class ButtonComponent {
   readonly type = input<'button' | 'submit'>('button');
   readonly color = input<'primary' | 'accent'>('primary');
   readonly variant = input<'normal' | 'outlined'>('normal');
+  readonly disabled = input<boolean>(false);
+  readonly fluid = input<boolean>(false);
+  readonly rounded = input<'2xl' | '3xl'>('2xl');
 
   private _designMap() {
     return {
@@ -23,14 +26,26 @@ export class ButtonComponent {
       },
       outlined: {
         primary:
-          'text-primary-300 ring-4 ring-primary-700 hover:text-primary-50 hover:bg-primary-700 active:ring-primary-800 active:bg-primary-800 focus:inset-ring-2 focus:inset-ring-primary-700',
+          'text-primary-300 ring-4 ring-primary-700 hover:text-primary-50 hover:bg-primary-700 active:ring-primary-800 active:bg-primary-800 focus:inset-ring-2 focus:inset-ring-primary-700 disabled:bg-primary-900 disabled:ring-0',
         accent:
-          'text-accent-50 ring-4 ring-accent-700 hover:text-accent-50 hover:bg-accent-700 active:ring-accent-800 active:bg-accent-800 focus:inset-ring-2 focus:inset-ring-accent-700',
+          'text-accent-50 ring-4 ring-accent-700 hover:text-accent-50 hover:bg-accent-700 active:ring-accent-800 active:bg-accent-800 focus:inset-ring-2 focus:inset-ring-accent-700 disabled:bg-primary-900 disabled:ring-0',
       },
     };
   }
 
+  private _mapFluid() {
+    return this.fluid() ? 'w-full' : '';
+  }
+
+  private _mapRounded() {
+    return `rounded-${this.rounded()}`;
+  }
+
   get design() {
-    return this._designMap()[this.variant()][this.color()];
+    const c = this._designMap()[this.variant()][this.color()];
+    const f = this._mapFluid();
+    const r = this._mapRounded();
+
+    return `${c} ${f} ${r}`;
   }
 }
