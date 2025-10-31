78992efe7df0f0d3c56b075dc3afaa8f4e008d83
Alben Bustamante <albenbustamante@gmail.com>
2025-10-31 18:23:49 -0500

    update: padding buttom value

--- src/app/components/container/container.component.ts
+++ src/app/components/container/container.component.ts
@@ -8,5 +8,5 @@ import { Component, input } from '@angular/core';
   styleUrl: './container.component.css',
 })
 export class ContainerComponent {
-  readonly verticalPadding = input<string>('py-10');
+  readonly verticalPadding = input<string>('py-14');
 }
