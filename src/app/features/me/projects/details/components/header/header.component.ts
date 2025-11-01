fce35036cfeb8e958b0de0030203b893f37917ed
Alben Bustamante <albenbustamante@gmail.com>
2025-11-01 17:38:11 -0500

    feat: skills section

--- src/app/features/me/projects/details/components/header/header.component.ts
+++ src/app/features/me/projects/details/components/header/header.component.ts
@@ -2,11 +2,17 @@ import { Component, input } from '@angular/core';
 import { TitleComponent } from '../title/title.component';
 import { OpenLiveDemoComponent } from '../open-live-demo/open-live-demo.component';
 import { SourceCodeButtonComponent } from '../source-code-button/source-code-button.component';
-import { ProjectDetailModel } from '../../../../../../core/models/project-detail.model';
+import { ProjectDetailModel } from '@core/models/project-detail.model';
+import { HeaderSectionComponent } from '../header-section/header-section.component';
 
 @Component({
   selector: 'app-header',
-  imports: [TitleComponent, OpenLiveDemoComponent, SourceCodeButtonComponent],
+  imports: [
+    TitleComponent,
+    OpenLiveDemoComponent,
+    SourceCodeButtonComponent,
+    HeaderSectionComponent,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
