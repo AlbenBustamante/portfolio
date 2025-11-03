b2facd16b35db45f3e4060756e7537fa639aae3f
Alben Bustamante <albenbustamante@gmail.com>
2025-11-03 16:26:53 -0500

    update: project detail header responsive design

--- src/app/features/me/projects/details/details.component.ts
+++ src/app/features/me/projects/details/details.component.ts
@@ -2,10 +2,17 @@ import { Component, inject, input } from '@angular/core';
 import { Store } from './store';
 import { ContainerComponent } from '@components/container/container.component';
 import { HeaderComponent } from './components/header/header.component';
+import { RightArrowIconComponent } from '../components/right-arrow-icon/right-arrow-icon.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-details',
-  imports: [ContainerComponent, HeaderComponent],
+  imports: [
+    ContainerComponent,
+    HeaderComponent,
+    RightArrowIconComponent,
+    RouterLink,
+  ],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css',
   providers: [Store],
