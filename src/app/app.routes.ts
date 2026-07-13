809ed516ab99d82f53eacf18c485918f8dfe65f9
Alben Bustamante <albenbustamante@gmail.com>
2026-07-13 01:10:47 -0400

    remove: old version, page-not-found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,10 +6,6 @@ export const routes: Routes = [
     loadChildren: () => import('./features/me/me.routes'),
   },
   {
-    path: 'old',
-    loadChildren: () => import('./features/old/old.routes'),
-  },
-  {
     path: '**',
     redirectTo: '',
   },
--- src/app/features/old/v1/landing/resume/skills/skills.data.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable, signal } from '@angular/core';
-import { Skill } from '@features/old/v1/core/models/skill.model';
-
-@Injectable({
-  providedIn: 'root',
-})
-export class SkillsData {
-  readonly skills = signal<Skill[]>([]);
-}
