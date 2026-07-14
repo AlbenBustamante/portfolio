62c49fe95a08964b466f18a9c5bd0135b35d3993
Alben Bustamante <albenbustamante@gmail.com>
2026-07-14 01:07:31 -0400

    remove: unused translator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,12 @@
 import { Component } from '@angular/core';
-import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '@components/navbar/navbar.component';
 import { FooterComponent } from '@components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [TranslateModule, RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  constructor(private readonly _translate: TranslateService) {
-    this._translate.addLangs(['es', 'en']);
-    this._translate.setDefaultLang('es');
-    this._translate.use(this._translate.getBrowserLang() || 'es');
-  }
-}
+export class AppComponent {}
